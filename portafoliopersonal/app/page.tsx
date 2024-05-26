74a9815ca113df3c0b51b93a73857d1bb193a785
Joaquin Campos <98112929+JoaquinCampos22155@users.noreply.github.com>
2024-05-25 18:13:49 -0600

    objeto Particles

--- portafoliopersonal/app/page.tsx
+++ portafoliopersonal/app/page.tsx
@@ -1,10 +1,10 @@
 import Image from "next/image";
-
+import CoverParticles from "@/components/cover-particles";
 export default function Home() {
   return (
     <main>
       <div className="flex min-h-[100vh] h-full bg-no-repeat bg-gradient-cover">
-        <p>Cover</p>
+        <CoverParticles />
         <p>Introduccion</p>
 
       </div>
