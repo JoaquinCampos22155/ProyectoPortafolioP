a0b43e6f680f16218f82bd1d02d256a60169d30f
Joaquin Campos <98112929+JoaquinCampos22155@users.noreply.github.com>
2024-05-27 11:06:06 -0600

    Animaciones y componente introduction

--- portafoliopersonal/app/page.tsx
+++ portafoliopersonal/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import CoverParticles from "@/components/cover-particles";
 import TransitionPage from "@/components/transition-page";
+import Introduction from "@/components/introduction";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <TransitionPage />
       <div className="flex min-h-[100vh] h-full bg-no-repeat bg-gradient-cover">
         <CoverParticles />
+        <Introduction />
 
       </div>
     </main>
