66a7bbe1b2b005996308dda8389a081206767b76
Joaquin Campos <98112929+JoaquinCampos22155@users.noreply.github.com>
2024-05-27 13:03:04 -0600

    Home bruto

--- portafoliopersonal/components/introduction.tsx
+++ portafoliopersonal/components/introduction.tsx
@@ -1,7 +1,51 @@
+'use client'
+import { Radius } from 'lucide-react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { TypeAnimation } from 'react-type-animation';
+
 const Introduction = () => {
     return ( 
-        <div>
-            Introduction
+        <div className='z-20 w-full bg-darkBg/60'>
+            <div className='z-20 grid items-center h-full p-6 py-20 md:py-0 md:grid-cols-2'>
+            <Image  className= "justify-center" src="/home-4.jpeg" priority width="300" height="300" alt='Profile pic' style={{borderRadius: "50%"}}/>
+            <div className='flex flex-col justify-center max-w-md'>
+                <h1 className='mb-5 text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-10'>
+                    Y pensar que todo comenzó con, 
+                    <TypeAnimation sequence={[
+                        " Scratch",
+                        1000,
+                        ' "Es por dinero"',
+                        500,
+                        " inspeccionar elemento",
+                        1000,
+                        ' Greenfoot',
+                        1000,
+
+                    ]}
+                    wrapper='span'
+                    speed={50}
+                    repeat={Infinity}
+                    className='font-bold text-secondary'/>
+                </h1>
+                <h2 className='mb-5 text-4xl leading-tight text-center md:text-left md:text-4xl md:mb-10'>
+                    Joaquín Campos
+                </h2>
+                <h3 className='mb-5 text-2xl leading-tight text-center md:text-left md:text-2xl md:mb-10'>
+                    Estudiante de CC&TI - Universidad del Valle de Guatemala
+                </h3>
+                <div className='flex item-center justify-center gap-3 md:justify-start md:gap-10'>
+                    <Link href="about-me" className="px-3 py-2 transition-all border-2 cursor-pointer text-white border-secondary text-md w-fit rounded-xl hover:shadow-xl hover:shadow-secondary/50">
+                        SOBRE MI
+                    </Link>
+                    <Link href="projects" className="px-3 py-2 transition-all border-2 cursor-pointer text-white border-secondary text-md w-fit rounded-xl hover:shadow-xl hover:shadow-secondary/50">
+                        PROYECTOS
+                    </Link>
+
+                </div>
+            </div>
+            </div>
+            
         </div>
      );
 }
--- portafoliopersonal/data.tsx
+++ portafoliopersonal/data.tsx
@@ -1,31 +1,22 @@
-import { BookText, CodeSquare, HomeIcon, UserRound, Linkedin, Twitter, Rss, Twitch, Youtube, Crop, Pencil, Computer, Book, Rocket, Speech } from "lucide-react";
+import { BookText, CodeSquare, HomeIcon,Instagram,Github, Brain,  UserRound, Linkedin, Twitter, Rss, Twitch, Youtube, Crop, Pencil, Computer, Book, Rocket, Speech } from "lucide-react";
 
 export const socialNetworks = [
     {
         id: 1,
-        logo: <Youtube size={30} strokeWidth={1} />,
+        logo: <Instagram size={40} strokeWidth={1} />,
         src: "#!",
     },
     {
         id: 2,
-        logo: <Linkedin size={30} strokeWidth={1} />,
+        logo: <Linkedin size={40} strokeWidth={1} />,
         src: "#!",
     },
     {
         id: 3,
-        logo: <Twitter size={30} strokeWidth={1} />,
+        logo: <Github size={40} strokeWidth={1} />,
         src: "#!",
     },
-    {
-        id: 4,
-        logo: <Rss size={30} strokeWidth={1} />,
-        src: "#!",
-    },
-    {
-        id: 5,
-        logo: <Twitch size={30} strokeWidth={1} />,
-        src: "#!",
-    },
+    
 ];
 
 
@@ -46,20 +37,15 @@ export const itemsNavbar = [
         id: 3,
         title: "Book",
         icon: <BookText size={25} color="#fff" strokeWidth={1} />,
-        link: "/services",
+        link: "/projects",
     },
     {
         id: 4,
         title: "Target",
-        icon: <CodeSquare size={25} color="#fff" strokeWidth={1} />,
-        link: "/portfolio",
+        icon: <Brain size={25} color="#fff" strokeWidth={1} />,
+        link: "/technologies",
     },
-    {
-        id: 5,
-        title: "Home",
-        icon: <Speech size={25} color="#fff" strokeWidth={1} />,
-        link: "/testimonials",
-    },
+
 ];
 
 export const dataAboutPage = [
@@ -211,47 +197,3 @@ export const dataPortfolio = [
     }
 ];
 
-export const dataTestimonials = [
-    {
-        id: 1,
-        name: "George Snow",
-        description:
-            "¡Increíble plataforma! Los testimonios aquí son genuinos y me han ayudado a tomar decisiones informadas. ¡Altamente recomendado!",
-        imageUrl: "/profile1.png",
-    },
-    {
-        id: 2,
-        name: "Juan Pérez",
-        description:
-            "Me encanta la variedad de testimonios disponibles en esta página. Es inspirador ver cómo otras personas han superado desafíos similares a los míos. ¡Gracias por esta invaluable fuente de motivación!",
-        imageUrl: "/profile2.png",
-    },
-    {
-        id: 3,
-        name: "María García",
-        description:
-            "Excelente recurso para obtener opiniones auténticas sobre diferentes productos y servicios. Me ha ayudado mucho en mis compras en línea. ¡Bravo por este sitio!",
-        imageUrl: "/profile3.png",
-    },
-    {
-        id: 4,
-        name: "Laura Snow",
-        description:
-            "¡Qué descubrimiento tan fantástico! Los testimonios aquí son honestos y detallados. Me siento más seguro al tomar decisiones después de leer las experiencias compartidas por otros usuarios.",
-        imageUrl: "/profile4.png",
-    },
-    {
-        id: 5,
-        name: "Carlos Sánchez",
-        description:
-            "Una joya en la web. Los testimonios son fáciles de encontrar y están bien organizados. ¡Definitivamente mi destino número uno cuando necesito referencias confiables!",
-        imageUrl: "/profile5.png",
-    },
-    {
-        id: 6,
-        name: "Antonio Martínez",
-        description:
-            "¡Fantástico recurso para aquellos que buscan validación antes de tomar decisiones importantes! Los testimonios aquí son veraces y realmente útiles. ¡Gracias por simplificar mi proceso de toma de decisiones!",
-        imageUrl: "/profile6.png",
-    },
-];
