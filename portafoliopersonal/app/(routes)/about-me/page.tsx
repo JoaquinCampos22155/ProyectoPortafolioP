c030a27ff96011d5a8677aff6b7f260369646ac3
Joaquin Campos <98112929+JoaquinCampos22155@users.noreply.github.com>
2024-05-28 00:04:09 -0600

    about-me terminado

--- portafoliopersonal/app/(routes)/about-me/page.tsx
+++ portafoliopersonal/app/(routes)/about-me/page.tsx
@@ -14,6 +14,13 @@ const PageAboutMe = () => {
                 </span>
             </h1>
             <CounterServices />
+            <p className="max-w-xxl text-2xl text-justify">
+                Joaquin Campos, actualmente cursando 3er año de la carrera ciencias de la computación y tecnologías de la información, 
+                dentro de los proyectos que he trabajado la mayoría han sido trabajos de la universidad, de las cuales solos 3 han tenido 
+                product-owners, pero dejando de lado, la experiencia como ingeniero en CC&TI ha sido gratificante y les presento por este 
+                medio algunos de los proyectos que he tenido el placer de poder hacerlos públicos, sobre todo también tengo algunos proyectos
+                no públicos de los cuales me gustaría poder enseñar algún día. 
+            </p>
         </ContainerPage>
         </>
      );
--- portafoliopersonal/data.tsx
+++ portafoliopersonal/data.tsx
@@ -41,65 +41,34 @@ export const itemsNavbar = [
     },
     {
         id: 4,
-        title: "Target",
+        title: "Tec",
         icon: <Brain size={25} color="#fff" strokeWidth={1} />,
         link: "/technologies",
     },
 
 ];
 
-export const dataAboutPage = [
-    {
-        id: 1,
-        title: "Frontend Developer",
-        subtitle: "TechSolutions",
-        description: "Colabora con un equipo dinámico para desarrollar interfaces de usuario atractivas y funcionales que impulsen el éxito de nuestros clientes en el mundo digital.",
-        date: "Nov 2023 ",
-    },
-    {
-        id: 2,
-        title: "Creador de Experiencias Digitales",
-        subtitle: "PixelCrafters",
-        description: "Trabaja en proyectos emocionantes que desafían los límites de la creatividad y la tecnología. Únete a nosotros mientras creamos experiencias digitales cautivadoras que inspiran y cautivan a nuestros usuarios.",
-        date: "May 2021",
-    },
-    {
-        id: 3,
-        title: "Especialista en Desarrollo Frontend",
-        subtitle: "CodeForge Solutions",
-        description: "Como desarrollador frontend, tendrás la oportunidad de colaborar en proyectos diversos y desafiantes que te permitirán expandir tus habilidades y dejar tu huella en el mundo digital.",
-        date: "Ago 2019",
-    },
-    {
-        id: 4,
-        title: "Prácticas Grado",
-        subtitle: "WebWizards Inc.",
-        description: "Únete a nosotros mientras creamos sitios web y aplicaciones interactivas que sorprenden y deleitan a nuestros clientes. Si tienes pasión por el diseño y la programación, y disfrutas colaborar en un entorno creativo, ¡queremos conocerte!        ",
-        date: "Mar 2018",
-    },
-]
-
 export const dataCounter = [
     {
         id: 0,
-        endCounter: 10,
+        endCounter: 4,
         text: "Años de experiencia",
         lineRight: true,
         lineRightMobile: true,
     },
     {
         id: 1,
-        endCounter: 80,
+        endCounter: 3,
         text: "Clientes satisfechos",
         lineRight: true,
         lineRightMobile: false,
     },
     {
         id: 2,
-        endCounter: 220,
+        endCounter: 6,
         text: "Proyectos finalizados",
-        lineRight: true,
-        lineRightMobile: true,
+        lineRight: false,
+        lineRightMobile: false,
     },
 ];
 
